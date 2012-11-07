1408876446d551fcaa62e4e54a70016711883838
Mark <mark@Jeeves.(none)>
2012-11-06 19:06:16 -0600

    Yay! It's now actually Conway's game! Looping is broken. Need to fix that.

--- game.py
+++ game.py
@@ -30,7 +30,8 @@ class Game():
 		pygame.init()
 		self.mouseclick = False
 		self.timer = pygame.time.Clock()
-		self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+		#self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+		self.screen = pygame.display.set_mode((800,200))
 		self.width, self.height = self.screen.get_size()
 		self.interesting = dict()
 		self.temp = dict()
@@ -48,57 +49,53 @@ class Game():
 		#find a nice way to determine how many o the surrounding squares are 1. that is all.
 		#Return from that the number. Other method can use it.
 		#print([a for a in range(x-1,x+2) for b in range(y-1,y+2) if (a,b) in self.interesting and self.interesting[(a,b)] is True])
-		return sum(1 for a in range(x-1,x+2) for b in range(y-1,y+2) if (a,b) in self.interesting and self.interesting[(a,b)] is True)
-		
-		pass
+		temp = sum(1 for a in range(x-1,x+2) for b in range(y-1,y+2) if (a,b) in self.interesting and self.interesting[(a,b)] is True)
+		if self.interesting[pos]:
+			return temp - 1
+		else: return temp
 
 	def drawThing(self, pos):
 		x, y = pos
-		x1 = (x+1)%self.width
+		x1 = (x+2)%self.width
 		y1 = (y+1)%self.height
 		self.turnOnSquare((x,y))
 		self.turnOnSquare((x1,y))
 		self.turnOnSquare((x,y1))
-		self.turnOnSquare((x1,y1))
+		self.turnOnSquare((x1+1,y1))
 	
 	def turnOnSquare(self, pos):
 		x, y = pos
-		self.pixArray[x][y] = WHITE
-		self.temp.update(((i,j),False) for i in range(x-1, x+2) for j in range(y-1,y+2) if not (i,j) in self.temp)
+		self.pixArray[x%self.width][y%self.height] = WHITE
+		self.temp.update(((i%self.width,j%self.height),False) for i in range(x-1, x+2) for j in range(y-1,y+2) if not ((i,j) in self.temp or (i,j) in self.interesting))
 		self.temp[pos] = True
 
 	def turnOffSquare(self, pos):
 		x,y = pos
-		self.pixArray[x][y] = BLACK
+		self.pixArray[x%self.width][y%self.height] = BLACK
 		self.temp[pos] = False
  
 	def update(self):
 		self.pixArray = pygame.PixelArray(self.background)
 		remove = set()
+		print(len(self.interesting))
 		if self.mouseclick:
 			self.drawThing(pygame.mouse.get_pos())
 		else:
 			for pos, turnedOn in self.interesting.iteritems():
 				if turnedOn:
-					print(self.neighbors(pos))
 					if self.neighbors(pos) < 2:
-						print('dead')
 						self.turnOffSquare(pos)
 					elif self.neighbors(pos) > 3:
-						print('dead again')
 						self.turnOffSquare(pos)
 				else:
 					if(self.neighbors(pos) == 3):
-						print('alive!')
 						self.turnOnSquare(pos)
 					elif(self.neighbors(pos) == 0):
-						print('ignore')
 						remove.add(pos)
 			self.interesting.update(self.temp)
 			self.temp = dict()
 			for r in remove:
 				self.interesting.pop(r)
-
 		del self.pixArray
 
 	def draw(self):
